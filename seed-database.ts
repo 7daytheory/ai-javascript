64ca18e4a944bdf433ff0a3395ee9ec9db7553fe
Matthew Lowe <matthewjlowe11@gmail.com>
2025-06-26 11:55:44 -0400

    Seperate employee schema data into TS file, create inferred type as Employee

--- seed-database.ts
+++ seed-database.ts
@@ -4,6 +4,7 @@ import { MongoClient } from "mongodb";
 import { MongoDBAtlasVectorSearch } from "@langchain/mongodb"; // Crete embeddings and save into mongodb
 import { z } from "zod"; //More type safety
 import "dotend/config"; //.env file
+import { Employee, EmployeeSchema } from "./employeeSchema"; 
 
 const client = new MongoClient(process.env.MONGODB_ATLAS_URI as string);
 
@@ -11,5 +12,3 @@ const llm = new ChatOpenAI({
     modelName: "gpt-4o-mini",
     temperature: 0.7, //0 is most strict - 1 is more creative
 })
-
-//Create dummy data 
