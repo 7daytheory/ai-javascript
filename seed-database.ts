f0fa06bf7a739ac60652bbda58efc636e6bfddb2
Matthew Lowe <matthewjlowe11@gmail.com>
2025-06-26 12:53:41 -0400

    Fix: 1 minor syntax error, 1 spelling mistake

--- seed-database.ts
+++ seed-database.ts
@@ -3,7 +3,7 @@ import { StructuredOutputParser } from "@langchain/core/output_parsers"; // Ensu
 import { MongoClient } from "mongodb";
 import { MongoDBAtlasVectorSearch } from "@langchain/mongodb"; // Crete embeddings and save into mongodb
 import { z } from "zod"; //More type safety
-import "dotend/config"; //.env file
+import "dotenv/config"; //.env file
 import { Employee, EmployeeSchema } from "./employeeSchema"; 
 
 const client = new MongoClient(process.env.MONGODB_ATLAS_URI as string);
@@ -56,7 +56,7 @@ async function seedDatabase(): Promise<void> {
         const db= client.db("hr_database");
         const collection = db.collection("employees");
 
-        await collection.deleteMany([]); //Clear existiong data
+        await collection.deleteMany({}); //Clear existing data
 
         const syntheticData = await generateSyntheticData();
 
